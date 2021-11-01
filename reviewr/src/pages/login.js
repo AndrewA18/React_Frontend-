37ab4a83740a2194b46e432a48a63507e6d830d1
AndrewA18 <78102921+AndrewA18@users.noreply.github.com>
2021-10-31 21:48:02 -0400

    Update login.js

--- reviewr/src/pages/login.js
+++ reviewr/src/pages/login.js
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 
-function login(){
+function Login(){
+    const [email, setEmail]= useState([]);
+    const [password, setPassword]= useState([]);
+
+    let item = {email, password};
+
+    async function loggin ()
+    {
+        var formData = new FormData();
+        formData.append('email', email);
+        formData.append('password', password);
+        let result = fetch("/api/login/", {
+            method: 'POST',
+            header:{
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: formData
+        });
+        console.log(result);
+        //history.push("/") trying to get it to route to the homepage after login
+    }
+    
+    
+    
     return (
         <div>
             <h1>Login Page </h1>
             <div className="col-sm-6 offset-sm-3">
-            <input type="text" placeholder="Email" className="form-control" />
+            <input type="text" placeholder="Email" className="form-control"
+                onChange={(e)=>setEmail(e.target.value)}/> 
             <br />
-            <input type="password" placeholder="Password" className="form-control" />
+            <input type="password" placeholder="Password" className="form-control" 
+                onChange={(e)=>setPassword(e.target.value)}/>
             <br />
-            <button className="btn btn-primary" > Login   </button>
+            <button onClick={loggin} className="btn btn-primary" > Login   </button>
             <button className="btn btn-primary" > Register</button>
             </div>
         </div>
     )
 }
-export default login
+export default Login
