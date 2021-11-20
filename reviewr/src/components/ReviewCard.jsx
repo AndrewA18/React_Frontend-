bd42a6b8da57b70e3373fba5116c019ed979a4e7
Jgeissler14 <66270580+Jgeissler14@users.noreply.github.com>
2021-11-19 19:34:18 -0500

    Update ReviewCard.jsx

--- reviewr/src/components/ReviewCard.jsx
+++ reviewr/src/components/ReviewCard.jsx
@@ -4,6 +4,13 @@ import React from 'react';
 
 function ReviewCard(review) {
 
+  const handleUpVote = () => {
+    console.log("Up")
+  }
+  const handleDownVote = () => {
+    console.log("Down")
+  }
+
   return(
     <Box p="4" maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
       <Box p="2">
@@ -17,6 +24,14 @@ function ReviewCard(review) {
           {review.text} 
         </Box>
       </Box>
+<<<<<<< Updated upstream
+=======
+      <Flex>
+          <Button onClick={handleUpVote}>{String.fromCharCode(unfilledUpArrow)}</Button>
+          <Spacer />
+          <Button onClick={handleDownVote}>{String.fromCharCode(unfilledDownArrow)}</Button>
+      </Flex>
+>>>>>>> Stashed changes
     </Box>
   )
 }
