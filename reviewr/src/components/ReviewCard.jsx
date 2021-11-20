160b2c2d98366cc9027224cb17574067274669dc
Derek Ledford <Lehquack@gmail.com>
2021-11-19 19:49:04 -0500

    Making us able to use hooks in the cards

--- reviewr/src/components/ReviewCard.jsx
+++ reviewr/src/components/ReviewCard.jsx
@@ -1,6 +1,6 @@
 import { Box, Button, Flex, Spacer} from "@chakra-ui/react";
 
-import React from 'react';
+import React, {useState} from 'react';
 
 const unfilledUpArrow = 9651;
 const unfilledDownArrow = 9661;
@@ -8,6 +8,9 @@ const unfilledStar = 9734;
 const filledStar = 9733;
 
 function ReviewCard(review) {
+
+  const [upvoted, setUpvoted] = useState(false);
+  const [downvoted, setDownvoted] = useState(false);
 
   return(
     <Box p="4" mt={2} maxW="sm" borderWidth="1px" borderRadius="lg">
@@ -20,18 +23,18 @@ function ReviewCard(review) {
           alignItems
         >
           <Flex>
-          {review.user.name}
+          {review.props.user.name}
           <Spacer/>
-          {String.fromCharCode(filledStar).repeat(review.rating)}
+          {String.fromCharCode(filledStar).repeat(review.props.rating)}
           </Flex>
           <br/>
-          {review.text} 
+          {review.props.text} 
           <br/>
           <br/>
         </Box>
       </Box>
       <Flex>
-          <Button>{String.fromCharCode(unfilledUpArrow)}</Button>
+          <Button color={upvoted ? "blue" : "red"} onClick={() => setUpvoted(!upvoted)}>{String.fromCharCode(unfilledUpArrow)}</Button>
           <Spacer />
           <Button>{String.fromCharCode(unfilledDownArrow)}</Button>
       </Flex>
